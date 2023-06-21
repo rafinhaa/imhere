a0f656153c8601f761313a1366215168bc2ae073
Rafinhaa <rafinhaa@me.com>
2023-06-21 20:06:04 -0300

    Fundamentos - Input de dados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { styles } from "./styles";
 
 const Home: FC = () => {
@@ -7,6 +7,11 @@ const Home: FC = () => {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do evento"
+        placeholderTextColor={"#6B6B6B"}
+      />
     </View>
   );
 };
