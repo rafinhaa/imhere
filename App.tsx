19254441d0d8d0749fcce5111d0741f15db0eaa0
Rafinhaa <rafinhaa@me.com>
2023-06-21 19:46:48 -0300

    Fundamentos - Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,24 +1,31 @@
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const App: FC = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "#131016", padding: 24 }}>
-      <Text
-        style={{
-          color: "#FDFCFE",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 48,
-        }}
-      >
-        Nome do evento
-      </Text>
-      <Text style={{ color: "#6b6b6b", fontSize: 16 }}>
-        Sexta, 4 de novembro de 2022!
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022!</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+});
