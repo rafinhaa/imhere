aa5f6c0aa53701195a8aeece72c5730de44d698a
Rafinhaa <rafinhaa@me.com>
2023-06-22 20:18:44 -0300

    Fundamentos - Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import { FC } from "react";
 import { Home } from "./src/screens";
+import { StatusBar, View } from "react-native";
 
 const App: FC = () => {
-  return <Home />;
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <Home />
+    </View>
+  );
 };
 
 export default App;
